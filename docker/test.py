eb54c7e3292253aa45ccbee5eb5019741997c88f
admin <datnguyentran8@ggmail.com>
2024-10-31 15:16:16 +0700

    test

--- docker/test.py
+++ docker/test.py
@@ -1,25 +1,31 @@
-import tritonclient.http as httpclient
+import requests
 import numpy as np
+import json
 
 def test_triton_model():
-    url = "localhost:8000"
-    model_name = "qwen"
-
-
-    client = httpclient.InferenceServerClient(url=url)
-
+    url = "http://localhost:8000/v2/models/qwen/infer"
     input_text = "test 123 tesst -- 11 -- ++."
-    input_data = np.array([input_text], dtype="object")
 
-    input_tensor = httpclient.InferInput("INPUT", input_data.shape, "BYTES")
-    input_tensor.set_data_from_numpy(input_data)
+    input_data = np.array([input_text], dtype="object").tolist()
 
-    response = client.infer(
-        model_name=model_name,
-        inputs=[input_tensor]
-    )
+    payload = {
+        "inputs": [
+            {
+                "name": "INPUT",
+                "shape": [1],
+                "datatype": "BYTES",
+                "data": input_data
+            }
+        ],
+        "outputs": [{"name": "OUTPUT"}]
+    }
 
-    output_data = response.as_numpy("OUTPUT")
-    print("Output từ mô hình Triton:", output_data[0].decode("utf-8"))
+    response = requests.post(url, json=payload, timeout=3600)
+
+    if response.status_code == 200:
+        output_data = response.json()["outputs"][0]["data"][0]
+        print("Output từ mô hình Triton:", output_data)
+    else:
+        print("Lỗi:", response.status_code, response.text)
 
 test_triton_model()
