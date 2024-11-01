a2c2fe5f2e3637a70b4538143cb871a7afe5eb50
admin <datnguyentran8@ggmail.com>
2024-11-01 08:07:25 +0700

    test

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers.generation import GenerationConfig
 import torch
+from vllm import LLM, SamplingParams
 
 
-torch.manual_seed(1234)
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-Audio", trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-Audio", device_map="cpu", trust_remote_code=True, bf16=True).eval()
-model.generation_config = GenerationConfig.from_pretrained("Qwen/Qwen-Audio", trust_remote_code=True)
+prompts = [
+    "Hello, my name is",
+    "The president of the United States is",
+    "The capital of France is",
+    "The future of AI is",
+]
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 
-audio_url = "./data/dia1_utt0.wav"
-sp_prompt = "<|startoftranscription|><|en|><|transcribe|><|en|><|notimestamps|><|wo_itn|>"
-query = f"<audio>{audio_url}</audio>{sp_prompt}"
+llm = LLM(model="Qwen/Qwen-7B")
 
 
-audio_info = tokenizer.process_audio(query)
-inputs = tokenizer(query, return_tensors='pt', audio_info=audio_info)
-inputs = inputs.to(model.device)
-pred = model.generate(**inputs, audio_info=audio_info)
-response = tokenizer.decode(pred.cpu()[0], skip_special_tokens=False,audio_info=audio_info)
-print(response)
+outputs = llm.generate(prompts, sampling_params)
+
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
